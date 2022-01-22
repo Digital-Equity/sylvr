78aa147c59f57e80f813a51906e15e92e1cf3b6e
chainreaction6 <blora616@gmail.com>
2022-01-22 11:56:14 -0500

    narrow down

--- contracts/interfaces/IEscrow.sol
+++ contracts/interfaces/IEscrow.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "./IERC20.sol";
-
 interface IEscrow {
-    function deposit(IERC20 token, uint256 amount) external returns(uint256);
+    function deposit(uint256 amount) external returns(uint256);
     function withdraw() external returns (uint256);
     function payout() external returns (uint256);
     event Deposit(uint256 amount, address from);
-    event Withdraw(uint256 amount, address to);
+    event Withdrawal(uint256 amount, address to);
 }
--- contracts/interfaces/ITrust.sol
+++ contracts/interfaces/ITrust.sol
@@ -4,10 +4,14 @@ pragma solidity ^0.8.10;
 import "./IERC20.sol";
 
 interface ITrust {
-    function setAdmin(address admin) external returns (bool);
+    function setAdmin(address admin) external;
+    function revokeRights() external returns(address);
+    function payout(address token, uint256 amount) external;
+    function deposit(address token, uint256 amount) external returns (uint256);
     function getBalanceForToken(address token) external view returns (uint256);
-    // function getTokenReserves() external view returns (bytes32[] memory);
-    function deposit(IERC20 token, uint256 amount) external returns (uint256);
-    event Deposit(IERC20 token, uint256 amount);
-    event Payment(IERC20 token, uint256 amount);
+
+    event Deposit(address token, uint256 amount);
+    event Payment(address token, uint256 amount);
+    event AdminAssigned(address newAdmin, address prevAdmin);
+    event AdminRemoved(address removedAdmin, address currentAdmin);
 }
