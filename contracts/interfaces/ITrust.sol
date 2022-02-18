41426da0ffc6d196320b00f6efcc2e1f5d3d6401
chainreaction6 <blora616@gmail.com>
2022-02-18 17:54:11 -0500

    clone capability

--- contracts/interfaces/ITrust.sol
+++ contracts/interfaces/ITrust.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-
 interface ITrust {
     function deposit(address token, uint256 amount) external;
 
-    function payoutEth(uint256 amount) external payable;
+    function payout(address token, uint256 amount)
+        external
+        returns (uint256 remainingBal);
 
-    function payout(address token, uint256 amount) external returns (uint256 remainingBal);
+    function withdrawBenefactor(address token, uint256 amount)
+        external
+        returns (uint256 remainingBal);
 
-    function withdrawBenefactor(address token, uint256 amount) external returns (uint256 remainingBal);
-
-    function withdrawBeneficiary(address token, uint256 amount) external returns (uint256 remainingBal);
-
-    function withdrawETHBenefactor(uint256 amount) external returns (uint256 remainingBal);
-
-    function withdrawETHBeneficiary(uint256 amount) external returns (uint256 remainingBal);
+    function withdrawBeneficiary(address token, uint256 amount)
+        external
+        returns (uint256 remainingBal);
 
     event Deposit(address indexed token, address indexed from, uint256 amount);
     event Payment(address indexed token, address indexed to, uint256 amount);
