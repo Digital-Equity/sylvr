b6afbb3536d1334e07e017d0d4b9a5d9858d77d7
chainreaction6 <blora616@gmail.com>
2022-06-10 14:44:49 -0400

    testing vault contract

--- test/vault.test.js
+++ test/vault.test.js
@@ -24,7 +24,29 @@ contract("Vault", ([dev, user]) => {
   it("Should send 50 shares to depositor upon deposit", async () => {
     await this.vault.deposit(this.deposit, { from: user });
     let shares = await this.vault.balanceOf.call(user);
+    let endBalance = await this.sylvr.balanceOf.call(user);
 
+    assert.equal("0", endBalance.toString());
     assert.equal(shares.toString(), this.deposit.toString());
+  });
+
+  it("Should emit a withdrawal event upon withdrawal", async () => {
+    await this.vault.deposit(this.deposit, { from: user });
+    let receipt = await this.vault.withdraw(this.deposit, { from: user });
+
+    expectEvent(receipt, "Withdrawal", { to: user, amount: this.deposit });
+  });
+
+  it("Should burn 50 shares and return 50 sylvr tokens to depositor", async () => {
+    await this.vault.deposit(this.deposit, { from: user });
+    await this.vault.withdraw(this.deposit, { from: user });
+
+    // should be 50 after prior withdrawal
+    let balance = await this.sylvr.balanceOf.call(user);
+    // should be 0 after user withdrawal
+    let totalSupply = await this.vault.totalSupply.call();
+
+    assert.equal(balance.toString(), this.deposit.toString());
+    assert.equal(totalSupply, "0");
   });
 });
